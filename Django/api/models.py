b951e8b4d324012d885f76a3b328551edb0616ce
Neel Mehta <neelmehta247@gmail.com>
2017-01-21 14:53:15 -0500

    Added new user code

--- Django/api/models.py
+++ Django/api/models.py
@@ -10,12 +10,29 @@ from django.db import models
 class University(models.Model):
     name = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     university = models.ForeignKey(University)
+    id = models.CharField(max_length=36, default=uuid.uuid4, primary_key=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def json(self):
+        return {'username': self.user.username, 'email': self.user.email, 'name': self.user.first_name,
+                'id': str(self.id), 'university': self.university.__str__(), 'university_id': self.university.pk}
 
 
 class Session(models.Model):
     session_token = models.CharField(max_length=36, default=uuid.uuid4)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.session_token) + ' ' + str(self.user.__str__())
+
+    def json(self):
+        return {'session_token': str(self.session_token), 'user': self.user.json()}
--- Django/api/views.py
+++ Django/api/views.py
@@ -1,14 +1,42 @@
-from django.core import serializers
-from django.http import HttpResponse
+import json
 
-from .models import University
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
+
+from .models import University, UserProfile, Session
 
 
 # Create your views here.
 
 
 def new_user(request):
-    return ''
+    name = request.POST.get('name', False)
+    email = request.POST.get('email', False)
+    password = request.POST.get('password', False)
+    university = request.POST.get('university', False)
+
+    if not name or not email or not password or not university:
+        return HttpResponseBadRequest()
+
+    else:
+        user = User.objects.create_user(email, email, password, first_name=name)
+        user.save()
+
+        user_profile = UserProfile.objects.create(user=user, university=University.objects.get(pk=university))
+        user_profile.save()
+
+        user = authenticate(username=email, password=password)
+
+        if user is not None:
+            session = Session(user=user_profile)
+            session.save()
+
+            return HttpResponse(json.dumps(session.json()),
+                                content_type='application/json; charset=utf8')
+        else:
+            return HttpResponseServerError
 
 
 def all_universities(request):
