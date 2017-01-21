162245295349fada8dbce7072739ed4c97b823bb
Neel Mehta <neelmehta247@gmail.com>
2017-01-21 15:22:40 -0500

    Configure and push to heroku

--- Django/backend/wsgi.py
+++ Django/backend/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django.settings")
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
