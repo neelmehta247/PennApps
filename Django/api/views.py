95062de2babb9e8d48379d57e9e3d35e3ebcb9d1
Neel Mehta <neelmehta247@gmail.com>
2017-01-21 11:47:04 -0500

    did some commits

--- Django/api/views.py
+++ Django/api/views.py
@@ -1,5 +1,6 @@
 from django.core import serializers
 from django.http import JsonResponse
+from django.http import HttpResponse
 
 from .models import University
 
@@ -11,5 +12,6 @@ def new_user(request):
     return ""
 
 
-def all_universities():
-    return JsonResponse(serializers.serialize('json', University.objects.all()))
+def all_universities(request):
+    #return JsonResponse(serializers.serialize('json', University.objects.all()))
+    return HttpResponse(len(University.objects.all()))
